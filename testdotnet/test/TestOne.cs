e5c19bbdc09884ef1441e3915d259ca37a3da539
Loa-cloud <More4014@yandex.ru>
2023-12-05 10:47:05 +0300

    1
    1

---
+++ testdotnet/test/TestOne.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using System;
+
+namespace TestOne
+{
+    public class Test : IDisposable
+    {
+
+        public Test()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            int a = 1;
+            int b = 1;
+            Assert.Equal(a, b);
+        }
+    }
+}
